6a5e56000c506aa376c49a05fbd424a48519a749
mike.chang <s33165309000@gmail.com>
2024-08-05 17:50:36 +0800

    feat: add spinning functionality to the wheel component

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,24 @@
-import { useState } from 'react';
-import './App.css';
+import { FC, useState } from 'react';
+import Wheel from './components/Wheel';
 import { Button } from '@/components/ui/button';
-import { Loader2 } from 'lucide-react';
 
-function App() {
+const App: FC = () => {
+  const [rotation, setRotation] = useState(0);
+
+  const spin = () => {
+    const newRotation = rotation + Math.floor(Math.random() * 360 + 720); // 隨機旋轉720到1080度
+    setRotation(newRotation);
+  };
+
   return (
-    <>
-      <Button variant="ghost" className="">
-        <Loader2 className="mr-2 h-4 w-4 animate-spin" />
-        Click me
-      </Button>
-    </>
+    <div className="flex flex-col items-center">
+      <Wheel rotation={rotation} />
+      <button onClick={spin} className="mt-4 px-4 py-2 rounded">
+        旋轉
+      </button>
+      <Button onClick={spin} >Button</Button>
+    </div>
   );
-}
+};
 
 export default App;
