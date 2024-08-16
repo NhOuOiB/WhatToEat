e47568c5eae93c22f393fed537f3548d2cc9519c
mike.chang <s33165309000@gmail.com>
2024-08-16 17:58:11 +0800

    feat: Refactor App component to use SettingPanel for segment selection and add spinning functionality to Wheel component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { FC, useState } from 'react';
 import Wheel from './components/Wheel';
-import { Button } from '@/components/ui/button';
-import { Select, SelectContent, SelectGroup, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select';
+import SettingPanel from './components/SettingPanel';
 
 const App: FC = () => {
   const [rotation, setRotation] = useState<number>(0);
@@ -26,40 +25,13 @@ const App: FC = () => {
   };
 
   return (
-    <div className="h-screen flex flex-col justify-center items-center gap-10 bg-gray-200">
-      <Select onValueChange={(value) => setSegments(value)} defaultValue={segments}>
-        <SelectTrigger className="w-[140px]">
-          <SelectValue placeholder="選擇項目數量" />
-        </SelectTrigger>
-        <SelectContent>
-          {[...Array(8).keys()].map(
-            (i) =>
-              i !== 0 && (
-                <SelectGroup key={i}>
-                  <SelectItem value={`${i + 1}`}>{i + 1}</SelectItem>
-                </SelectGroup>
-              )
-          )}
-        </SelectContent>
-      </Select>
-      <div className={`text-6xl font-bold`}>
-        抽到 <span className={`${selectedItem === 0 && 'opacity-0'}`}>{selectedItem}</span> 啦 !
-      </div>
-      <div className="flex flex-col items-center gap-1">
-        <div className=" w-6 h-10 relative flex justify-center">
-          <div className="w-0 h-full border border-sky-800 absolute"></div>
-          <div className="w-full border border-b-0 border-x-[0.8rem] border-x-transparent border-t-[.8rem] border-sky-800 absolute bottom-0"></div>
-          <div className="w-full border border-b-0 border-x-[0.8rem] border-x-transparent border-t-[.6rem] border-gray-200 absolute bottom-1"></div>
+    <div className="h-screen flex justify-center items-center gap-10 bg-gray-200">
+      <SettingPanel spin={spin} reset={reset} setSegments={setSegments} segments={segments} />
+      <div className="flex flex-col justify-center items-center gap-20">
+        <div className={`text-6xl font-bold`}>
+          抽到 <span className={`${selectedItem === 0 && 'opacity-0'}`}>{selectedItem}</span> 啦 !
         </div>
         <Wheel rotation={rotation} segments={segments} />
-      </div>
-      <div className='flex gap-6'>
-        <Button className="p-8 text-xl" onClick={spin}>
-          旋轉
-        </Button>
-        <Button className="p-8 text-xl" onClick={reset}>
-          重置
-        </Button>
       </div>
     </div>
   );
