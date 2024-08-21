514d6f503dacb5534d9acb933afd95757a7959f5
NhOuOiB <s33165309000@gmail.com>
2024-08-21 22:17:47 +0800

    feat: Update SettingPanel component with responsive layout and additional options

--- src/components/settingPanel/SettingPanel.tsx
+++ src/components/settingPanel/SettingPanel.tsx
@@ -12,7 +12,7 @@ interface SettingPanelProps {
 
 const SettingPanel: React.FC<SettingPanelProps> = ({ setSegments, segments }) => {
   return (
-    <div className="w-1/5 h-5/6 p-10 grid grid-cols-1 grid-rows-7 bg-[#ffffff] rounded-3xl shadow-md">
+    <div className="w-full md:w-1/3 xl:w-1/4 2xl:w-1/4 min-[1980px]:w-1/5 h-5/6 p-10 flex flex-col gap-4 sm:gap-10 bg-[#ffffff] rounded-3xl shadow-md">
       <div className="flex flex-col gap-4">
         <Label htmlFor="">轉盤樣式</Label>
         <div className="grid grid-cols-2 gap-2">
@@ -25,35 +25,24 @@ const SettingPanel: React.FC<SettingPanelProps> = ({ setSegments, segments }) =>
         </div>
       </div>
       <div className="flex flex-col gap-4">
-        <Label htmlFor="">轉盤樣式</Label>
-        <div className="grid grid-cols-8 gap-2">
-          <Button className="px-6 py-4 text-2xl">1</Button>
-          <Button className="px-6 py-4 text-2xl">2</Button>
-          <Button className="px-6 py-4 text-2xl">3</Button>
-          <Button className="px-6 py-4 text-2xl">4</Button>
-          <Button className="px-6 py-4 text-2xl">5</Button>
-          <Button className="px-6 py-4 text-2xl">6</Button>
-          <Button className="px-6 py-4 text-2xl">7</Button>
-          <Button className="px-6 py-4 text-2xl">8</Button>
+        <Label htmlFor="">轉盤內容</Label>
+        <div className="grid grid-cols-4 gap-2">
+          <Button className="px-6 py-4 text-md">美食</Button>
+          <Button className="px-6 py-4 text-md">店家</Button>
         </div>
       </div>
       <div className="flex flex-col gap-4">
         <Label htmlFor="count">項目數量</Label>
-        <Select onValueChange={(value) => setSegments(value)} defaultValue={segments}>
-          <SelectTrigger id="count" className="w-3/4 h-[2.4rem]">
-            <SelectValue placeholder="選擇項目數量" />
-          </SelectTrigger>
-          <SelectContent>
-            {[...Array(8).keys()].map(
-              (i) =>
-                i !== 0 && (
-                  <SelectGroup key={i}>
-                    <SelectItem value={`${i + 1}`}>{i + 1}</SelectItem>
-                  </SelectGroup>
-                )
-            )}
-          </SelectContent>
-        </Select>
+        <div className="grid grid-cols-4 sm:grid-cols-8 md:grid-cols-2 xl:grid-cols-4 2xl:grid-cols-6 min-[2560px]:grid-cols-8 gap-2">
+          {[...Array(8).keys()].map(
+            (i) =>
+              i !== 0 && (
+                <Button className="px-6 py-4 text-2xl" onClick={() => setSegments(`${i + 1}`)}>
+                  {i + 1}
+                </Button>
+              )
+          )}
+        </div>
       </div>
     </div>
   );
