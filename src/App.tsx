1e20ff448ec768856040ff41fb9c8f9df95c6c8d
NhOuOiB <s33165309000@gmail.com>
2024-08-25 23:13:15 +0800

    feat: Update App component with new WheelPanel types and condition state

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,24 @@
 import { FC, useEffect, useState } from 'react';
 import SettingPanel from './components/settingPanel/SettingPanel';
 import WheelPanel from './components/wheelPanel/WheelPanel';
+import VerticalWheelPanel from './components/wheelPanel/VerticalWheelPanel.tsx';
+import HorizontalWheelPanel from './components/wheelPanel/HorizontalWheelPanel.tsx';
 import { Condition } from './types/type.ts';
 
 const App: FC = () => {
+  const [condition, setCondition] = useState<Condition>({ segments: 2, distance: 2000 });
+  const [wheelType, setWheelType] = useState<string>('verticalWheel');
+
+  // Wheel
   const [rotation, setRotation] = useState<number>(0);
-  const [segments, setSegments] = useState<string>('2');
   const [selectedItem, setSelectedItem] = useState<number>(0);
-  const [condition, setCondition] = useState<Condition>({segments: 2, distance: 200});
 
   const spin = () => {
     const newRotation = rotation - Math.floor(Math.random() * 360 + 3600);
     setRotation(newRotation);
 
     // 計算指針指到的項目
-    const itemAngle = 360 / Number(segments); // 每個項目的角度範圍
+    const itemAngle = 360 / Number(condition.segments); // 每個項目的角度範圍
     const relativeRotation = (Math.abs(newRotation) + itemAngle / 2) % 360; // 計算多餘的旋轉角度，+ itemAngle / 2 是因為指針指到正中間
     const selectedItem = Math.floor(relativeRotation / itemAngle); // 計算指針指到的項目索引
 
@@ -23,17 +27,23 @@ const App: FC = () => {
 
   useEffect(() => {
     setSelectedItem(0);
-  }, [segments]);
+  }, [condition.segments]);
 
   return (
     <div className="h-screen flex flex-col md:flex-row justify-center items-center gap-10 bg-gray-200 px-6 py-2">
-      <SettingPanel setSegments={setSegments} condition={condition} setCondition={setCondition} />
-      <WheelPanel
-        rotation={rotation}
+      <SettingPanel
         condition={condition}
-        selectedItem={selectedItem}
-        spin={spin}
+        setCondition={setCondition}
+        wheelType={wheelType}
+        setWheelType={setWheelType}
       />
+      {wheelType === 'wheel' ? (
+        <WheelPanel rotation={rotation} condition={condition} selectedItem={selectedItem} spin={spin} />
+      ) : wheelType === 'verticalWheel' ? (
+        <VerticalWheelPanel condition={condition} />
+      ) : (
+        <HorizontalWheelPanel condition={condition} />
+      )}
     </div>
   );
 };
