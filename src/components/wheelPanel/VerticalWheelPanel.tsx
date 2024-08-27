0b22cf2d4fc1ab79a24d199896b9454c6bc0e76c
mike.chang <s33165309000@gmail.com>
2024-08-27 17:50:36 +0800

    feat: Update VerticalWheelPanel component with additional content and styling

--- src/components/wheelPanel/VerticalWheelPanel.tsx
+++ src/components/wheelPanel/VerticalWheelPanel.tsx
@@ -8,9 +8,11 @@ interface VerticalWheelPanelProps {
 
 const VerticalWheelPanel: React.FC<VerticalWheelPanelProps> = ({ condition }) => {
   return (
-    <div className="w-full md:w-1/2 xl:w-1/2 2xl:w-1/2 min-[1980px]:w-1/3 h-5/6 shadow rounded-3xl flex justify-center items-center">
+    <div className="w-full md:w-1/2 xl:w-1/2 2xl:w-1/2 min-[1980px]:w-1/3 h-5/6 shadow rounded-3xl flex flex-col justify-center items-center">
       <VerticalWheel condition={condition} />
-      <div>123</div>
+      <div className='w-36 h-36 rounded-full flex justify-center items-center shadow-2xl cursor-pointer'>
+        <div className='w-28 h-28 border rounded-full shadow-inner'></div>
+      </div>
     </div>
   );
 };
