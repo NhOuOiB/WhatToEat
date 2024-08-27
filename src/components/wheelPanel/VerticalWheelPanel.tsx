5cad2be0e194914d8e41802fab00a12e4ea18586
NhOuOiB <s33165309000@gmail.com>
2024-08-27 22:04:12 +0800

    feat: Update VerticalWheelPanel component with new styles and content

--- src/components/wheelPanel/VerticalWheelPanel.tsx
+++ src/components/wheelPanel/VerticalWheelPanel.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import VerticalWheel from '../wheel/VerticalWheel';
 import { Condition } from '@/types/type';
+import style from './VerticalWheelPanel.module.scss';
 
 interface VerticalWheelPanelProps {
   condition: Condition;
@@ -8,10 +9,10 @@ interface VerticalWheelPanelProps {
 
 const VerticalWheelPanel: React.FC<VerticalWheelPanelProps> = ({ condition }) => {
   return (
-    <div className="w-full md:w-1/2 xl:w-1/2 2xl:w-1/2 min-[1980px]:w-1/3 h-5/6 shadow rounded-3xl flex flex-col justify-center items-center">
+    <div className={style.container}>
       <VerticalWheel condition={condition} />
-      <div className='w-36 h-36 rounded-full flex justify-center items-center shadow-2xl cursor-pointer'>
-        <div className='w-28 h-28 border rounded-full shadow-inner'></div>
+      <div className={style.btnOut}>
+        <div className={style.btnIn}></div>
       </div>
     </div>
   );
