883c92f59cc576027a980194c1c186d8eb357012
mike.chang <s33165309000@gmail.com>
2024-08-28 17:41:03 +0800

    feat: Update VerticalWheel module and VerticalWheel component styles

--- src/components/wheel/VerticalWheel.tsx
+++ src/components/wheel/VerticalWheel.tsx
@@ -1,26 +1,41 @@
-import React from 'react';
+import { useRef, useEffect } from 'react';
 import { Condition } from '@/types/type';
 import style from './VerticalWheel.module.scss';
 
 interface VerticalWheelProps {
   condition: Condition;
+  itemAngle: number;
+  rotateDeg: number;
+  setRotateDeg: React.Dispatch<React.SetStateAction<number>>;
 }
 
-const VerticalWheel: React.FC<VerticalWheelProps> = ({ condition }) => {
-  const itemAngle = 360 / condition.segments;
+const VerticalWheel: React.FC<VerticalWheelProps> = ({
+  condition,
+  itemAngle,
+  rotateDeg,
+  setRotateDeg,
+}) => {
   const translateZ = 160 / 2 / Math.tan((itemAngle / 2 / 180) * Math.PI);
-  const rotate = (Math.random() * 360) + (360 * 5)
+  const wheelRef = useRef<HTMLDivElement>(null);
+  useEffect(() => {
+    setRotateDeg(rotateDeg - (rotateDeg % itemAngle));
+    wheelRef.current?.style.setProperty('transition', 'ease-in-out 0.5s');
+    setTimeout(() => {
+      wheelRef.current?.style.setProperty('transition', 'ease-in-out 4s');
+    }, 500);
+  }, [condition.segments]);
   return (
-    <div className="w-3/4 h-2/3 flex justify-center items-center">
-      <div className={style.container} style={{transform: ` rotateY(-15deg)`}}>
+    <div className={style.container}>
+      <div className={style.blur}></div>
+      <div className={style.wheel} style={{ transform: `rotateX(${rotateDeg}deg)` }} ref={wheelRef}>
         {Array.from({ length: condition.segments }).map((_, index) => {
           return (
             <div
-              className={style.wheel}
+              className={style.leaf}
               style={{ transform: `rotateX(${index * itemAngle}deg) translateZ(${translateZ}px) ` }}
               key={index}
             >
-              <p>{index + 1}</p>
+              <p className="text-3xl font-extrabold">{index + 1}</p>
             </div>
           );
         })}
