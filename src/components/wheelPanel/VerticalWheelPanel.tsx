0d5cb34cd8ee170e1d356b449fa9bf79c543b794
mike.chang <s33165309000@gmail.com>
2024-08-28 17:41:07 +0800

    feat: Update VerticalWheelPanel component with gap between items and new spin functionality

--- src/components/wheelPanel/VerticalWheelPanel.tsx
+++ src/components/wheelPanel/VerticalWheelPanel.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import VerticalWheel from '../wheel/VerticalWheel';
 import { Condition } from '@/types/type';
 import style from './VerticalWheelPanel.module.scss';
@@ -8,10 +8,19 @@ interface VerticalWheelPanelProps {
 }
 
 const VerticalWheelPanel: React.FC<VerticalWheelPanelProps> = ({ condition }) => {
+  const [rotateDeg, setRotateDeg] = React.useState(0);
+  const itemAngle = 360 / condition.segments;
+  const spin = () => {
+    let randomDeg = Math.random() * 360 + 360 * 5;
+    randomDeg -= randomDeg % itemAngle;
+
+    setRotateDeg(rotateDeg - randomDeg);
+  };
+  
   return (
     <div className={style.container}>
-      <VerticalWheel condition={condition} />
-      <div className={style.btnOut}>
+      <VerticalWheel condition={condition} itemAngle={itemAngle} rotateDeg={rotateDeg} setRotateDeg={setRotateDeg} />
+      <div className={style.btnOut} onClick={spin}>
         <div className={style.btnIn}></div>
       </div>
     </div>
