eafeaf7eb63f077387d4ec9795996b1b59f1fe6d
mike.chang <s33165309000@gmail.com>
2024-08-29 17:53:02 +0800

    feat: Update App component to use lazy loading for images

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,38 @@
-import { FC } from 'react';
-import FirstPage from './page/FirstPage.tsx';
+import { FC, useEffect, useRef } from 'react';
+import FirstPage from './page/FirstPage';
+import SecondPage from './page/SecondPage';
+import './App.css';
 
 const App: FC = () => {
+  // 讓滾輪更平順
+  const containerRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const handleWheel = (event: WheelEvent) => {
+      event.preventDefault();
+      containerRef.current?.scrollBy({
+        top: event.deltaY,
+        behavior: 'smooth'
+      });
+    };
+
+    const container = containerRef.current;
+    container?.addEventListener('wheel', handleWheel);
+
+    return () => {
+      container?.removeEventListener('wheel', handleWheel);
+    };
+  }, []);
 
   return (
-    <>
-      <FirstPage />
-    </>
+    <div className="snap-y-mandatory" ref={containerRef}>
+      <div className="snap-start">
+        <FirstPage />
+      </div>
+      <div className="snap-start">
+        <SecondPage />
+      </div>
+    </div>
   );
 };
 
