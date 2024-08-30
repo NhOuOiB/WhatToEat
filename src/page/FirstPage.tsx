864a91c7a1b50e3f93557b2696b2fc18d6449585
mike.chang <s33165309000@gmail.com>
2024-08-30 17:10:04 +0800

    feat: Improve geolocation functionality and integrate Google Places API

--- src/page/FirstPage.tsx
+++ src/page/FirstPage.tsx
@@ -6,17 +6,19 @@ import HorizontalWheelPanel from '../components/wheelPanel/HorizontalWheelPanel.
 import { Condition } from '../types/type.ts';
 import axios from 'axios';
 import { google_key } from '../../utils/config.ts';
+import { Place } from '../types/type.ts';
 
 const FirstPage: FC = () => {
   const [wheelType, setWheelType] = useState<string>('verticalWheel');
   const [condition, setCondition] = useState<Condition>({
     segments: wheelType === 'wheel' ? 2 : wheelType === 'verticalWheel' ? 6 : 11,
     distance: 2000,
+    rankPreference: 'POPULARITY',
   });
 
   // Wheel
   const [rotation, setRotation] = useState<number>(0);
-  const [selectedItem, setSelectedItem] = useState<number>(0);
+  const [selectedItem, setSelectedItem] = useState<number>(-1);
 
   const spin = () => {
     const newRotation = rotation - Math.floor(Math.random() * 360 + 3600);
@@ -27,63 +29,76 @@ const FirstPage: FC = () => {
     const relativeRotation = (Math.abs(newRotation) + itemAngle / 2) % 360; // 計算多餘的旋轉角度，+ itemAngle / 2 是因為指針指到正中間
     const selectedItem = Math.floor(relativeRotation / itemAngle); // 計算指針指到的項目索引
 
-    setSelectedItem(selectedItem + 1);
+    setSelectedItem(selectedItem);
+  };
+
+  // 取得使用者位置
+  const getLocation = async (): Promise<{ latitude: number; longitude: number }> => {
+    if (navigator.geolocation) {
+      return new Promise((resolve, reject) => {
+        navigator.geolocation.getCurrentPosition(
+          (position) => {
+            const location = {
+              latitude: position.coords.latitude,
+              longitude: position.coords.longitude,
+            };
+            resolve(location);
+          },
+          (error) => {
+            switch (error.code) {
+              case error.PERMISSION_DENIED:
+                console.log('使用者拒絕提供位置資訊。');
+                break;
+              case error.POSITION_UNAVAILABLE:
+                console.log('無法取得位置資訊。');
+                break;
+              case error.TIMEOUT:
+                console.log('取得位置資訊逾時。');
+                break;
+            }
+            reject(error);
+          }
+        );
+      });
+    } else {
+      console.log('瀏覽器不支援地理定位。');
+      throw new Error('瀏覽器不支援地理定位。');
+    }
   };
 
   // Google Places API
 
-  const [location, setLocation] = useState({ latitude: 0, longitude: 0 });
+  const [places, setPlaces] = useState<Place[]>([]);
 
-  function showPosition(position: GeolocationPosition) {
-    console.log('緯度: ' + position.coords.latitude);
-    console.log('經度: ' + position.coords.longitude);
-    setLocation({ latitude: position.coords.latitude, longitude: position.coords.longitude });
-    // 在這裡您可以將 position.coords.latitude 和 position.coords.longitude 傳給 Axios 請求
-  }
-
-  function showError(error: GeolocationPositionError) {
-    switch (error.code) {
-      case error.PERMISSION_DENIED:
-        console.log('使用者拒絕提供位置資訊。');
-        break;
-      case error.POSITION_UNAVAILABLE:
-        console.log('無法取得位置資訊。');
-        break;
-      case error.TIMEOUT:
-        console.log('取得位置資訊逾時。');
-        break;
-    }
-  }
-
-  const getLocation = async () => {
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(showPosition, showError);
-    } else {
-      console.log('瀏覽器不支援地理定位。');
-    }
-  };
-
-  const fetchData = async () => {
-    getLocation();
-    console.log(location);
-        const data = {
+  const fetchPlaces = async () => {
+    const location = await getLocation();
+    const data = {
       includedTypes: ['restaurant'],
-      maxResultCount: 10,
+      excludedTypes: [
+        'supermarket',
+        'park',
+        'zoo',
+        'amusement_park',
+        'aquarium',
+        'art_gallery',
+        'museum',
+      ],
+      maxResultCount: condition.segments,
       locationRestriction: {
         circle: {
           center: location,
-          radius: 1000.0,
+          radius: condition.distance,
         },
       },
       rankPreference: 'POPULARITY', // 依照熱門程度排名
     };
-    
+
     const headers = {
       'Content-Type': 'application/json',
       'X-Goog-Api-Key': google_key,
       'X-Goog-FieldMask': 'places.displayName,places.formattedAddress,places.types,places.rating', // 可自訂要顯示的欄位
     };
-    
+
     const response = await axios.post(
       'https://places.googleapis.com/v1/places:searchNearby',
       data,
@@ -91,20 +106,47 @@ const FirstPage: FC = () => {
         headers,
       }
     );
-    console.log(response.data);
+    // setPlaces(response.data.places);
+    setPlaces([
+      {
+        types: ['steak_house', 'restaurant', 'point_of_interest', 'food', 'establishment'],
+        formattedAddress: '235台灣新北市中和區建一路92號',
+        rating: 4.3,
+        displayName: {
+          text: '我家牛排 中和店',
+          languageCode: 'zh-TW',
+        },
+      },
+      {
+        types: [
+          'brunch_restaurant',
+          'breakfast_restaurant',
+          'chinese_restaurant',
+          'restaurant',
+          'point_of_interest',
+          'food',
+          'establishment',
+        ],
+        formattedAddress: '234台灣新北市永和區永和路二段284號',
+        rating: 3.8,
+        displayName: {
+          text: '世界豆漿大王',
+          languageCode: 'zh-TW',
+        },
+      },
+    ]);
+    // console.log(response.data.places);
   };
+
+  useEffect(() => {
+    fetchPlaces();
+  }, [condition]);
 
   useEffect(() => {
     setSelectedItem(0);
   }, [condition.segments]);
   return (
     <div className="w-full h-screen flex flex-col md:flex-row justify-center items-center gap-10 bg-gray-200 px-6 py-2 snap-start">
-      <div
-        className="p-2 border border-gray-800 rounded-lg cursor-pointer shadow-md hover:bg-gray-800 hover:text-white hover:shadow-xl trnasition duration-500 hover:scale-110"
-        onClick={fetchData}
-      >
-        click
-      </div>
       <SettingPanel
         condition={condition}
         setCondition={setCondition}
@@ -117,11 +159,12 @@ const FirstPage: FC = () => {
           condition={condition}
           selectedItem={selectedItem}
           spin={spin}
+          places={places}
         />
       ) : wheelType === 'verticalWheel' ? (
-        <VerticalWheelPanel condition={condition} />
+        <VerticalWheelPanel condition={condition} places={places} />
       ) : (
-        <HorizontalWheelPanel condition={condition} />
+        <HorizontalWheelPanel condition={condition} places={places} />
       )}
     </div>
   );
