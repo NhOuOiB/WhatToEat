1e7a4905130caacf885b3e31084fb6e5565a0c64
NhOuOiB <s33165309000@gmail.com>
2024-09-02 22:32:14 +0800

    feat: Add Opening component for initial screen display

---
+++ src/components/opening/Opening.tsx
@@ -0,0 +1,16 @@
+import { FC } from 'react';
+
+const Opening: FC = () => {
+  return (
+    <div className="w-full h-screen bg-white snap-start absolute z-10 flex justify-center items-center">
+      <div className="w-fit h-fit bg-gray-400">
+        <h1 className="text-[12rem]">Wel</h1>
+      </div>
+      <div>
+        <h1 className="text-[12rem]">come</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Opening;
