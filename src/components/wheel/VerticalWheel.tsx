939a15b466c20663ae6d2ef9af44091d8cce514d
mike.chang <s33165309000@gmail.com>
2024-09-03 17:55:18 +0800

    feat: Update VerticalWheel component to use selectedPlaces instead of places

--- src/components/wheel/VerticalWheel.tsx
+++ src/components/wheel/VerticalWheel.tsx
@@ -1,21 +1,22 @@
 import { useRef, useEffect } from 'react';
 import { Condition, Place } from '@/types/type';
 import style from './VerticalWheel.module.scss';
+import { Skeleton } from '@/components/ui/skeleton';
 
 interface VerticalWheelProps {
-  condition: Condition;
   itemAngle: number;
   rotateDeg: number;
   setRotateDeg: React.Dispatch<React.SetStateAction<number>>;
-  places: Place[];
+  selectedPlaces: Place[];
+  condition: Condition;
 }
 
 const VerticalWheel: React.FC<VerticalWheelProps> = ({
-  condition,
   itemAngle,
   rotateDeg,
   setRotateDeg,
-  places,
+  selectedPlaces,
+  condition,
 }) => {
   const translateZ = 160 / 2 / Math.tan((itemAngle / 2 / 180) * Math.PI);
   const wheelRef = useRef<HTMLDivElement>(null);
@@ -25,24 +26,32 @@ const VerticalWheel: React.FC<VerticalWheelProps> = ({
     setTimeout(() => {
       wheelRef.current?.style.setProperty('transition', 'ease-in-out 4s');
     }, 500);
-  }, [condition.segments]);
+  }, [selectedPlaces.length]);
   return (
     <div className={style.container}>
       <div className={style.blur}></div>
       <div className={style.wheel} style={{ transform: `rotateX(${rotateDeg}deg)` }} ref={wheelRef}>
-        {Array.from({ length: condition.segments }).map((_, index) => {
-          return (
-            <div
-              className={style.leaf}
-              style={{ transform: `rotateX(${index * itemAngle}deg) translateZ(${translateZ}px) ` }}
-              key={index}
-            >
-              <p className="text-3xl font-extrabold">
-                {places?.[index]?.displayName.text.split(' ')[0].split('-')[0]}
-              </p>
-            </div>
-          );
-        })}
+        {selectedPlaces.length < condition.min ? (
+          <div className={style.leaf}>
+            <Skeleton className="w-full h-full bg-yellow-50" />
+          </div>
+        ) : (
+          Array.from({ length: selectedPlaces.length }).map((_, index) => {
+            return (
+              <div
+                className={style.leaf}
+                style={{
+                  transform: `rotateX(${index * itemAngle}deg) translateZ(${translateZ}px) `,
+                }}
+                key={index}
+              >
+                <p className="text-3xl font-extrabold">
+                  {selectedPlaces?.[index]?.displayName.text.split(' ')[0].split('-')[0]}
+                </p>
+              </div>
+            );
+          })
+        )}
       </div>
     </div>
   );
