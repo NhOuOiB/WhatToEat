f60ca22e0ffe05db6c995ba6454a8c30fe73d3b7
mike.chang <s33165309000@gmail.com>
2024-09-03 17:55:33 +0800

    refactor: Update VerticalWheelPanel to use selectedPlaces instead of places

--- src/components/wheelPanel/VerticalWheelPanel.tsx
+++ src/components/wheelPanel/VerticalWheelPanel.tsx
@@ -5,23 +5,34 @@ import style from './VerticalWheelPanel.module.scss';
 
 interface VerticalWheelPanelProps {
   condition: Condition;
-  places: Place[];
+  selectedPlaces: Place[];
 }
 
-const VerticalWheelPanel: React.FC<VerticalWheelPanelProps> = ({ condition, places }) => {
+const VerticalWheelPanel: React.FC<VerticalWheelPanelProps> = ({ condition, selectedPlaces }) => {
   const [rotateDeg, setRotateDeg] = React.useState(0);
-  const itemAngle = 360 / condition.segments;
+  const itemAngle = 360 / selectedPlaces.length;
   const spin = () => {
     let randomDeg = Math.random() * 360 + 360 * 5;
     randomDeg -= randomDeg % itemAngle;
 
     setRotateDeg(rotateDeg - randomDeg);
   };
-  
+
   return (
     <div className={style.container}>
-      <VerticalWheel condition={condition} itemAngle={itemAngle} rotateDeg={rotateDeg} setRotateDeg={setRotateDeg} places={places} />
-      <div className={style.btnOut} onClick={spin}>
+      <VerticalWheel
+        itemAngle={itemAngle}
+        rotateDeg={rotateDeg}
+        setRotateDeg={setRotateDeg}
+        selectedPlaces={selectedPlaces}
+        condition={condition}
+      />
+      <div
+        className={style.btnOut}
+        onClick={() => {
+          if (selectedPlaces.length >= condition.min) spin();
+        }}
+      >
         <div className={style.btnIn}></div>
       </div>
     </div>
