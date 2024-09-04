5e50623320945ddc2c0a2e09619ada97736f67b3
mike.chang <s33165309000@gmail.com>
2024-09-04 17:59:10 +0800

    feat: Add geolocation functionality to App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { FC, useEffect, useRef } from 'react';
+import { FC, useState, useEffect, useRef } from 'react';
 import FirstPage from './page/FirstPage';
 import SecondPage from './page/SecondPage';
 import './App.css';
@@ -7,12 +7,53 @@ const App: FC = () => {
   // 讓滾輪更平順
   const containerRef = useRef<HTMLDivElement>(null);
 
+  // 取得使用者位置
+  const [location, setLocation] = useState<{ latitude: number; longitude: number }>({
+    latitude: 0,
+    longitude: 0,
+  });
+
+  useEffect(() => {
+    (async () => {
+      if (navigator.geolocation) {
+        return new Promise((resolve, reject) => {
+          navigator.geolocation.getCurrentPosition(
+            (position) => {
+              const location = {
+                latitude: position.coords.latitude,
+                longitude: position.coords.longitude,
+              };
+              setLocation(location);
+            },
+            (error) => {
+              switch (error.code) {
+                case error.PERMISSION_DENIED:
+                  console.log('使用者拒絕提供位置資訊。');
+                  break;
+                case error.POSITION_UNAVAILABLE:
+                  console.log('無法取得位置資訊。');
+                  break;
+                case error.TIMEOUT:
+                  console.log('取得位置資訊逾時。');
+                  break;
+              }
+              reject(error);
+            }
+          );
+        });
+      } else {
+        console.log('瀏覽器不支援地理定位。');
+        throw new Error('瀏覽器不支援地理定位。');
+      }
+    })();
+  }, []);
+
   useEffect(() => {
     const handleWheel = (event: WheelEvent) => {
       event.preventDefault();
       containerRef.current?.scrollBy({
         top: event.deltaY,
-        behavior: 'smooth'
+        behavior: 'smooth',
       });
     };
 
@@ -26,8 +67,8 @@ const App: FC = () => {
 
   return (
     <div className="snap-y-mandatory no-scrollbar" ref={containerRef}>
-        <FirstPage />
-        <SecondPage />
+      <FirstPage location={location} />
+      <SecondPage location={location} />
     </div>
   );
 };
