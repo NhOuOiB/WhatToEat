5de924f3459e1d867598884df5cf0e191e3baef4
mike.chang <s33165309000@gmail.com>
2024-09-04 17:59:29 +0800

    refactor: Update FirstPage component to use Props interface for location

--- src/page/FirstPage.tsx
+++ src/page/FirstPage.tsx
@@ -8,7 +8,11 @@ import { Condition } from '../types/type.ts';
 // import { google_key } from '../../utils/config.ts';
 import { Place } from '../types/type.ts';
 
-const FirstPage: FC = () => {
+interface Props {
+  location : { latitude: number; longitude: number };
+}
+
+const FirstPage: FC<Props> = ({ location }) => {
   const [wheelType, setWheelType] = useState<string>('verticalWheel');
   const [specialMode, setSpecialMode] = useState<boolean>(false);
   const [condition, setCondition] = useState<Condition>({
@@ -32,40 +36,6 @@ const FirstPage: FC = () => {
     const selectedItem = Math.floor(relativeRotation / itemAngle); // 計算指針指到的項目索引
 
     setSelectedItem(selectedItem);
-  };
-
-  // 取得使用者位置
-  const getLocation = async (): Promise<{ latitude: number; longitude: number }> => {
-    if (navigator.geolocation) {
-      return new Promise((resolve, reject) => {
-        navigator.geolocation.getCurrentPosition(
-          (position) => {
-            const location = {
-              latitude: position.coords.latitude,
-              longitude: position.coords.longitude,
-            };
-            resolve(location);
-          },
-          (error) => {
-            switch (error.code) {
-              case error.PERMISSION_DENIED:
-                console.log('使用者拒絕提供位置資訊。');
-                break;
-              case error.POSITION_UNAVAILABLE:
-                console.log('無法取得位置資訊。');
-                break;
-              case error.TIMEOUT:
-                console.log('取得位置資訊逾時。');
-                break;
-            }
-            reject(error);
-          }
-        );
-      });
-    } else {
-      console.log('瀏覽器不支援地理定位。');
-      throw new Error('瀏覽器不支援地理定位。');
-    }
   };
 
   // Google Places API
@@ -356,7 +326,6 @@ const FirstPage: FC = () => {
   const [selectedPlaces, setSelectedPlaces] = useState<Place[]>([]);
 
   const fetchPlaces = async () => {
-    const location = await getLocation();
     console.log(location);
     // const data = {
     //   includedTypes: ['restaurant'],
