9ca3916c9b6214cbf64100393210848c5a4c2a4e
mike.chang <s33165309000@gmail.com>
2024-09-12 17:54:34 +0800

    feat: Add Nutritionix API credentials to config.ts

--- utils/config.ts
+++ utils/config.ts
@@ -1,2 +1,4 @@
 export const google_key = import.meta.env.VITE_GOOGLE_KEY;
 export const map_id = import.meta.env.VITE_MAP_ID;
+export const nutritionix_id = import.meta.env.VITE_NUTRITIONIX_ID;
+export const nutritionix_key = import.meta.env.VITE_NUTRITIONIX_KEY;
