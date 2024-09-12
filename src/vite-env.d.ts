4c113441dcac58c15ee74178894cc2013f376145
mike.chang <s33165309000@gmail.com>
2024-09-12 17:54:15 +0800

    feat: Add environment variables for Nutritionix API credentials

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -3,6 +3,8 @@
 interface ImportMetaEnv {
   readonly VITE_APP_TITLE: string
   readonly VITE_GOOGLE_KEY: string
+  readonly VITE_NUTRITIONIX_ID: string
+  readonly VITE_NUTRITIONIX_KEY: string
 }
 
 interface ImportMeta {
