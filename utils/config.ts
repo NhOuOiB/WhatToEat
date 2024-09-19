7de93937abc1a059cf1903c4e6d2939f21f9fdae
mike.chang <s33165309000@gmail.com>
2024-09-19 17:56:48 +0800

    feat: Update config.ts with firebaseConfig

--- utils/config.ts
+++ utils/config.ts
@@ -2,3 +2,4 @@ export const google_key = import.meta.env.VITE_GOOGLE_KEY;
 export const map_id = import.meta.env.VITE_MAP_ID;
 export const nutritionix_id = import.meta.env.VITE_NUTRITIONIX_ID;
 export const nutritionix_key = import.meta.env.VITE_NUTRITIONIX_KEY;
+export const firebaseConfig = import.meta.env.VITE_FIREBASE_CONFIG;
