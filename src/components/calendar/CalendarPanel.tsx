a5d9d4450d653be6e684be98cff15283c9878caf
NhOuOiB <s33165309000@gmail.com>
2024-10-06 23:06:36 +0800

    refactor: Update CalendarPanel component to include setEdit prop

--- src/components/calendar/CalendarPanel.tsx
+++ src/components/calendar/CalendarPanel.tsx
@@ -1,4 +1,4 @@
-import { FC } from 'react';
+import React, { FC } from 'react';
 import { Calendar, momentLocalizer } from 'react-big-calendar';
 import moment from 'moment';
 
@@ -6,9 +6,10 @@ const localizer = momentLocalizer(moment);
 
 interface Props {
   event: { start: ''; end: ''; title: ''; calories: '' }[];
+  setEdit: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
-const CalendarPanel: FC<Props> = ({ event }) => {
+const CalendarPanel: FC<Props> = ({ event, setEdit }) => {
   return (
     <div className="w-5/6 sm:w-2/3 md:w-5/12 h-screen sm:h-2/3 border shadow rounded-xl p-4">
       <Calendar
@@ -17,9 +18,10 @@ const CalendarPanel: FC<Props> = ({ event }) => {
         events={event}
         onSelectEvent={(e) => {
           console.log(e);
+          setEdit(true);
         }}
         onSelectSlot={(slotInfo) => {
-          console.log(slotInfo);
+          console.log(slotInfo.slots);
         }}
         selectable
       />
