b6acb8f58cf73af2450ad7720dd8e98907a565e3
NhOuOiB <s33165309000@gmail.com>
2024-10-06 23:07:49 +0800

    refactor: Extract edit functionality in CaloriesPanel and CalendarPanel components

--- src/page/ThirdPage.tsx
+++ src/page/ThirdPage.tsx
@@ -1,14 +1,20 @@
 import { useState } from 'react';
 import CalendarPanel from '../components/calendar/CalendarPanel.tsx';
 import CaloriesPanel from '@/components/caloriesPanel/CaloriesPanel.tsx';
+import { CaloriesRecord } from '@/types/type';
 
 const ThirdPage = () => {
   const [event, setEvent] = useState<{ start: ''; end: ''; title: ''; calories: '' }[]>([]);
+  const [edit, setEdit] = useState<boolean>(false);
+  const [editData, setEditData] = useState<CaloriesRecord>({
+    title: '',
+    calories: '',
+  });
 
   return (
     <div className="sm:h-screen snap-start flex flex-col md:flex-row justify-center items-center sm:gap-6">
-      <CalendarPanel event={event} />
-      <CaloriesPanel setEvent={setEvent} />
+      <CalendarPanel event={event} setEdit={setEdit} />
+      <CaloriesPanel setEvent={setEvent} edit={edit} setEdit={setEdit} />
     </div>
   );
 };
