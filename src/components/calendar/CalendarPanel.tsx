77d86cb3792bffffd39ab9d1cfc6da46c9dc98a3
mike.chang <s33165309000@gmail.com>
2024-10-07 17:59:24 +0800

    refactor: Update CalendarPanel component to include setEditData, setEditDate, and setRecordList props

--- src/components/calendar/CalendarPanel.tsx
+++ src/components/calendar/CalendarPanel.tsx
@@ -1,15 +1,19 @@
 import React, { FC } from 'react';
 import { Calendar, momentLocalizer } from 'react-big-calendar';
 import moment from 'moment';
+import { CaloriesRecord } from '@/types/type';
 
 const localizer = momentLocalizer(moment);
 
 interface Props {
   event: { start: ''; end: ''; title: ''; calories: '' }[];
   setEdit: React.Dispatch<React.SetStateAction<boolean>>;
+  setEditData: React.Dispatch<React.SetStateAction<CaloriesRecord>>;
+  setEditDate: React.Dispatch<React.SetStateAction<Date | undefined>>;
+  setRecordList?: React.Dispatch<React.SetStateAction<CaloriesRecord[]>>;
 }
 
-const CalendarPanel: FC<Props> = ({ event, setEdit }) => {
+const CalendarPanel: FC<Props> = ({ event, setEdit, setEditData, setEditDate, setRecordList }) => {
   return (
     <div className="w-5/6 sm:w-2/3 md:w-5/12 h-screen sm:h-2/3 border shadow rounded-xl p-4">
       <Calendar
@@ -18,6 +22,8 @@ const CalendarPanel: FC<Props> = ({ event, setEdit }) => {
         events={event}
         onSelectEvent={(e) => {
           console.log(e);
+          setEditData(e);
+          setEditDate(moment(e.start).toDate());
           setEdit(true);
         }}
         onSelectSlot={(slotInfo) => {
