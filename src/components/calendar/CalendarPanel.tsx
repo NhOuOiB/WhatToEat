8d47647617938decca05e35ca08055f79c386e1e
mike.chang <s33165309000@gmail.com>
2024-10-08 17:59:17 +0800

    refactor: Update CalendarPanel component to include setSelectedDate prop and filter events on the same day

--- src/components/calendar/CalendarPanel.tsx
+++ src/components/calendar/CalendarPanel.tsx
@@ -6,14 +6,18 @@ import { CaloriesRecord } from '@/types/type';
 const localizer = momentLocalizer(moment);
 
 interface Props {
-  event: { start: ''; end: ''; title: ''; calories: '' }[];
+  event: { id: string; start: string; end: string; title: string; calories: string }[];
   setEdit: React.Dispatch<React.SetStateAction<boolean>>;
   setEditData: React.Dispatch<React.SetStateAction<CaloriesRecord>>;
   setEditDate: React.Dispatch<React.SetStateAction<Date | undefined>>;
-  setRecordList?: React.Dispatch<React.SetStateAction<CaloriesRecord[]>>;
+  setRecordList: React.Dispatch<React.SetStateAction<CaloriesRecord[]>>;
+  setSelectedDate: React.Dispatch<React.SetStateAction<Date>>;
 }
 
-const CalendarPanel: FC<Props> = ({ event, setEdit, setEditData, setEditDate, setRecordList }) => {
+const CalendarPanel: FC<Props> = ({ event, setEdit, setEditData, setEditDate, setRecordList, setSelectedDate }) => {
+  const findEventsOnSameDay = (date: Date) => {
+    return event.filter((e) => moment(e.start).isSame(date, 'day'));
+  };
   return (
     <div className="w-5/6 sm:w-2/3 md:w-5/12 h-screen sm:h-2/3 border shadow rounded-xl p-4">
       <Calendar
@@ -21,13 +25,16 @@ const CalendarPanel: FC<Props> = ({ event, setEdit, setEditData, setEditDate, se
         views={['month', 'week']}
         events={event}
         onSelectEvent={(e) => {
-          console.log(e);
           setEditData(e);
           setEditDate(moment(e.start).toDate());
           setEdit(true);
         }}
         onSelectSlot={(slotInfo) => {
-          console.log(slotInfo.slots);
+          const date = slotInfo.slots[0];
+          const eventsOnSameDay = findEventsOnSameDay(date);
+          setRecordList(eventsOnSameDay);
+          setSelectedDate(date);
+          setEdit(false);
         }}
         selectable
       />
