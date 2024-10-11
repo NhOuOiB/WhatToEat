e3c46207a0e864f98511a2ba16dfcf8f038eec54
mike.chang <s33165309000@gmail.com>
2024-10-11 17:52:24 +0800

    refactor: Update Nav component layout and styling

--- src/components/nav/Nav.tsx
+++ src/components/nav/Nav.tsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from 'react';
 
 const Nav = () => {
   return (
-    <div>Nav</div>
-  )
-}
+    <div className="w-fit h-fit absolute left-2 top-2 flex items-center">
+      <div className='w-10 h-10 border absolute rounded-full shadow -z-10 hidden'></div>
+      <div className="w-8 hover:w-96 h-8 border shadow rounded-full transition-all duration-300"></div>
+      <div className='w-10 h-10 border absolute right-0 rounded-full shadow -z-10 hidden'></div>
+    </div>
+  );
+};
 
-export default Nav
+export default Nav;
