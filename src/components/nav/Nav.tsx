8337a5b32ba584920a0ed34d7354bce86959f539
NhOuOiB <s33165309000@gmail.com>
2024-10-13 23:10:24 +0800

    refactor: update nav component style

--- src/components/nav/Nav.tsx
+++ src/components/nav/Nav.tsx
@@ -2,10 +2,19 @@ import React from 'react';
 
 const Nav = () => {
   return (
-    <div className="w-fit h-fit absolute left-2 top-2 flex items-center">
-      <div className='w-10 h-10 border absolute rounded-full shadow -z-10 hidden'></div>
-      <div className="w-8 hover:w-96 h-8 border shadow rounded-full transition-all duration-300"></div>
-      <div className='w-10 h-10 border absolute right-0 rounded-full shadow -z-10 hidden'></div>
+    <div className="w-fit h-fit absolute left-4 top-4 flex items-center">
+      <div className="w-12 hover:w-[17.4rem] h-12 border shadow rounded-full transition-all duration-300 bg-white flex items-center gap-2 overflow-hidden">
+        <div className="min-w-12 h-full rounded-full"></div>
+        <div className="border py-1 px-4 rounded-full text-nowrap cursor-pointer hover:shadow-inner">
+          轉盤
+        </div>
+        <div className="border py-1 px-4 rounded-full text-nowrap cursor-pointer hover:shadow-inner">
+          地圖
+        </div>
+        <div className="border py-1 px-4 rounded-full text-nowrap cursor-pointer hover:shadow-inner">
+          記錄
+        </div>
+      </div>
     </div>
   );
 };
