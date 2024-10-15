e5cab6571b5b9be02b3334ba87b49c10df43998c
NhOuOiB <s33165309000@gmail.com>
2024-10-15 23:04:59 +0800

    refactor: update scrollToRef function

--- src/components/nav/Nav.tsx
+++ src/components/nav/Nav.tsx
@@ -1,17 +1,39 @@
-import React from 'react';
+import { FC, useEffect } from 'react';
 
-const Nav = () => {
+interface Props {
+  firstPageRef: React.RefObject<HTMLDivElement>;
+  secondPageRef: React.RefObject<HTMLDivElement>;
+  thirdPageRef: React.RefObject<HTMLDivElement>;
+}
+
+const Nav: FC<Props> = ({ firstPageRef, secondPageRef, thirdPageRef }) => {
+  const scrollToRef = (ref: React.RefObject<HTMLDivElement>) => {
+    ref.current?.scrollIntoView({ behavior: 'smooth' });
+  };
   return (
     <div className="w-fit h-fit absolute left-4 top-4 flex items-center">
       <div className="w-12 hover:w-[17.4rem] h-12 border shadow rounded-full transition-all duration-300 bg-white flex items-center gap-2 overflow-hidden">
-        <div className="min-w-12 h-full rounded-full"></div>
-        <div className="border py-1 px-4 rounded-full text-nowrap cursor-pointer hover:shadow-inner">
+        <div className="min-w-12 h-full rounded-full flex flex-col justify-center items-center gap-1 overflow-hidden">
+          <div className='w-1/2 h-0.5 bg-slate-900'></div>
+          <div className='w-1/2 h-0.5 bg-slate-900'></div>
+          <div className='w-1/2 h-0.5 bg-slate-900'></div>
+        </div>
+        <div
+          className="border py-1 px-4 rounded-full text-nowrap cursor-pointer hover:shadow-inner"
+          onClick={() => scrollToRef(firstPageRef)}
+        >
           轉盤
         </div>
-        <div className="border py-1 px-4 rounded-full text-nowrap cursor-pointer hover:shadow-inner">
+        <div
+          className="border py-1 px-4 rounded-full text-nowrap cursor-pointer hover:shadow-inner"
+          onClick={() => scrollToRef(secondPageRef)}
+        >
           地圖
         </div>
-        <div className="border py-1 px-4 rounded-full text-nowrap cursor-pointer hover:shadow-inner">
+        <div
+          className="border py-1 px-4 rounded-full text-nowrap cursor-pointer hover:shadow-inner"
+          onClick={() => scrollToRef(thirdPageRef)}
+        >
           記錄
         </div>
       </div>
