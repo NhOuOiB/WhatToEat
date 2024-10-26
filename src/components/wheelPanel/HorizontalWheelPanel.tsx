fc27e4e8c2bd7a3728aef615a1ac8145b3268fbb
NhOuOiB <s33165309000@gmail.com>
2024-10-26 11:06:34 +0800

    refactor: Update HorizontalWheelPanel module styles

--- src/components/wheelPanel/HorizontalWheelPanel.tsx
+++ src/components/wheelPanel/HorizontalWheelPanel.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import HorizontalWheel from '../wheel/HorizontalWheel';
 import { Condition, Place } from '@/types/type';
+import style from './HorizontalWheelPanel.module.scss';
 
 interface HorizontalWheelPanelProps {
   condition: Condition;
@@ -9,7 +10,7 @@ interface HorizontalWheelPanelProps {
 
 const HorizontalWheelPanel: React.FC<HorizontalWheelPanelProps> = ({ condition, places }) => {
   return (
-    <div className="w-full md:w-1/2 xl:w-1/2 2xl:w-1/2 min-[1980px]:w-1/3 h-5/6 shadow rounded-3xl flex justify-center items-center">
+    <div className={style.container}>
       <HorizontalWheel condition={condition} places={places} />
     </div>
   );
